bf25a14fac9d89c36fdd426961b5a8bfa06926a9
Guilherme O <guilherme_otran@hotmail.com>
2015-09-15 18:03:32 -0300

    Oops

--- lib/cleardb-env2ssl.sh
+++ lib/cleardb-env2ssl.sh
@@ -1,8 +1,4 @@
-copy_from_env() {
-  echo $1 > $2
-}
-
-cd $HOME
-copy_from_env CLEARDB_SSL_CA_CERT cleardb-ca-cert.pem
-copy_from_env CLEARDB_SSL_CLI_CERT cleardb-cert.pem
-copy_from_env CLEARDB_SSL_KEY cleardb-key.pem
+cd $HOME &&
+echo "$CLEARDB_SSL_CA_CERT" > cleardb-ca-cert.pem &&
+echo "$CLEARDB_SSL_CLI_CERT" > cleardb-cert.pem &&
+echo "$CLEARDB_SSL_KEY" > cleardb-key.pem
